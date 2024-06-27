4f0ba26b2687a6ab76768657380421e29711d579
GustavoLyra23 <gustavolyra23@gmail.com>
2024-06-27 10:19:49 -0300

    first commit

---
+++ src/main/java/org/example/workshopmongodb/WorkshopmongodbApplication.java
@@ -0,0 +1,13 @@
+package org.example.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopmongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
