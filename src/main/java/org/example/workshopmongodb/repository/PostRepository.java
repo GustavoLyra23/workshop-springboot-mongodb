2468200d41fbafb525e5cd5bb837e78ea1abfce8
GustavoLyra23 <gustavolyra23@gmail.com>
2024-06-28 18:57:27 -0300

    @Query consult

--- src/main/java/org/example/workshopmongodb/repository/PostRepository.java
+++ src/main/java/org/example/workshopmongodb/repository/PostRepository.java
@@ -2,6 +2,7 @@ package org.example.workshopmongodb.repository;
 
 import org.example.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' }}")
+    List<Post> findByTitle(String title);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
--- src/main/java/org/example/workshopmongodb/services/PostService.java
+++ src/main/java/org/example/workshopmongodb/services/PostService.java
@@ -25,7 +25,7 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String title) {
-        return postRepository.findByTitleContainingIgnoreCase(title);
+        return postRepository.findByTitle(title);
     }
 
 }
